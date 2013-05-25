66cd4964af62faccc9e0934026099e3ca732acbd
Green Wang <cocowool@gmail.com>
2013-05-25 22:10:00 +0800

    Modify Admin Control Panel Style

--- application/views/manage/index.php
+++ application/views/manage/index.php
@@ -9,16 +9,23 @@
 	<div id="rc-wrap">
 		<div id="rc-adm-menu-wrap">
 			<ul id="rc-adm-menu">
-				<li><a href="#">控制面板</a></li>
-				<li><a href="#">首页</a></li>
-				<li><a href="#">文章</a></li>
-				<li><a href="#">媒体</a></li>
-				<li><a href="#">用户</a></li>
-				<li><a href="#">二维码</a></li>
+				<li><a class="menu-top" href="#">控制面板</a></li>
+				<li><a class="menu-top" href="#">首页</a></li>
+				<li><a class="menu-top" href="#">文章</a></li>
+				<li><a class="menu-top" href="#">媒体</a></li>
+				<li><a class="menu-top" href="#">用户</a></li>
+				<li><a class="menu-top" href="#">二维码</a></li>
 			</ul>
 		</div>
 		<div id="rc-content">
-			yyy
+			<div id="rc-head">
+				<h1 id="rc-site-title"><a href="#"><?php echo $this->config->item('adm_sys_title'); ?></a></h1>
+				<div id="rc-head-info">
+					<div id="rc-userinfo">
+						您好，<?php echo $this->session->userdata($this->config->item('adm_sess_username')); ?>
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
 </body>
