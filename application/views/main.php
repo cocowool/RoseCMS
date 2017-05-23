ea552d1f60bd62424e058c84a920f2c71164177f
Green Wang <cocowool@qq.com>
2017-05-23 17:38:25 +0800

    修改路由，调整默认首页

---
+++ application/views/main.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset='utf-8'>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>软考资料站</title>
+	<meta name="keyword" content="软考，系统分析师，系统架构师">
+	<meta name="description" content="系统分析师资料站">
+</head>
+<body>
+<h1>欢迎访问软考资料站</h1>
+</body>
+</html>
