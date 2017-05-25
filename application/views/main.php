8f4533d132616453d8de04eaffa3e18b7f6a37e0
Green Wang <cocowool@qq.com>
2017-05-25 17:08:11 +0800

    引入静态文件夹

--- application/views/main.php
+++ application/views/main.php
@@ -1,14 +1,16 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset='utf-8'>
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta charset='UTF-8'>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title>软考资料站</title>
 	<meta name="keyword" content="软考，系统分析师，系统架构师">
 	<meta name="description" content="系统分析师资料站">
+	<link rel='stylesheet' id='twentyseventeen-style-css'  href='/static/css/main.css' type='text/css' media='all' />
 </head>
 <body>
-<h1>欢迎访问软考资料站</h1>
+	
+	<h1>欢迎访问软考资料站</h1>
 </body>
 </html>
