ed6f4655ad6f02c7d01ac36b4a62217780072a2b
Green Wang <cocowool@qq.com>
2017-06-09 14:16:13 +0800

    Include bootstrap-3.3.7

--- application/views/manage/dashboard.php
+++ application/views/manage/dashboard.php
@@ -2,13 +2,35 @@
 <html>
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title><?php echo $this->config->item('site_name'); ?></title>
-
+	<link href="/static/lib/bootstrap-3.3.7/css/bootstrap.min.css" rel="stylesheet">
+	<script type="text/javascript" src="/static/lib/jquery/jquery-3.2.1.min.js"></script>
 </head>
 <body>
 	<?php
 		//$this->load->view('manage/manage_header');
-		
-	?>		
+
+	?>
+	<div id="rs-wrap">
+		<div id="rs-mainmenu">
+			<div>
+				<ul><li><a href="javascript:void(0);">发布</a></li></ul>
+			</div>
+		</div>
+		<div id="rs-content">
+			<div id="rs-topbar">
+				
+			</div>
+			<div id="rs-body">
+				<?php 
+				if(isset($default_view) and !empty($default_view)){
+					$this->load->view($default_view, $data);
+				}
+				?>
+			</div>
+		</div>
+	</div>		
 </body>
 </html>
