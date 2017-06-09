4e1b8b171ab90bf006a5a3e5ebd658b0f228e07c
Green Wang <cocowool@qq.com>
2017-06-09 16:33:52 +0800

    Modify Manage Style

--- application/views/manage/dashboard.php
+++ application/views/manage/dashboard.php
@@ -6,22 +6,25 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title><?php echo $this->config->item('site_name'); ?></title>
 	<link href="/static/lib/bootstrap-3.3.7/css/bootstrap.min.css" rel="stylesheet">
+	<link href="/static/admin/css/main.css" rel="stylesheet">
 	<script type="text/javascript" src="/static/lib/jquery/jquery-3.2.1.min.js"></script>
 </head>
-<body>
+<body class="container-fluid" id="rs-container">
 	<?php
 		//$this->load->view('manage/manage_header');
 
 	?>
 	<div id="rs-wrap">
 		<div id="rs-mainmenu">
-			<div>
+			<div id="rs-menu-wrap">
 				<ul><li><a href="javascript:void(0);">发布</a></li></ul>
 			</div>
 		</div>
 		<div id="rs-content">
 			<div id="rs-topbar">
-				
+				<ul>
+					<li><a href=""></a></li>
+				</ul>
 			</div>
 			<div id="rs-body">
 				<?php 
