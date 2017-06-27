0b13e2561934eac633fc36eefb08e9c01810d744
Green Wang <cocowool@qq.com>
2017-06-27 17:13:53 +0800

    Modify Footer

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,10 +1,21 @@
+	<div class="container">
 		<div class="row">
-			<div class="col-md-4">
-				
+			<div class="col-md-2">
+				<div class="rs-ft-logo">
+					<img src="/static/default/image/sa_white.png">
+				</div>				
 			</div>
-			<div class="col-md-4">
-				
+			<div class="col-md-3">
+				<div class="rs-ft-about">
+					<h3>关于我们</h3>
+					<p>我们致力于分享与计算机技术与软件专业技术资格（水平）考试相关的所有信息，如有需要，关注我们的微信公众号，随时随地掌握更多内容。</p>
+				</div>
 			</div>
-			<div class="col-md-4"></div>
-			<div class="col-md-4"></div>
-		</div>
+			<div class="col-md-3">
+				<div class="rs-ft-about">
+					<p><img src="/static/default/image/sapublic.jpg" width="129px"></p>					
+				</div>
+			</div>
+			<div class="col-md-3"></div>
+		</div>		
+	</div>
