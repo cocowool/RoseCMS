fc79244e3894e29e9e05383e076b0af690fd41ce
Green Wang <cocowool@qq.com>
2017-07-24 14:46:51 +0800

    Modify Upload

--- application/controllers/manage/Upload.php
+++ application/controllers/manage/Upload.php
@@ -16,42 +16,21 @@ class Upload extends MY_Controller {
 	}
 
 	public function index(){
-		$config['upload_path'] =	'upload';
+		$config['upload_path'] = 'data/' . date('Ymd', time());
+		$config['file_name'] = 'drill_' . $data['group'] . '_' . $data['position'] . '_' . time();
         $config['allowed_types'] = 	'gif|jpg|png';
         $config['max_size'] = 0;
         $config['max_height'] = 0;
         $config['max_width'] = 0;
 
 		$this->load->library('upload', $config);
-		
 		if(!$this->upload->do_upload('file')){
 			print_r($this->upload->display_errors());
 			die('{"jsonrpc" : "2.0", "error" : {"code": 101, "message": "Failed to open input stream."}, "id" : "id"}');
 		}else{
-			
 			$upload_data = $this->upload->data();
 			print_r($upload_data);
 		}
-		die;
-
-		@set_time_limit(5 * 60);
-
-
-
-                $this->load->library('upload', $config);
-
-                if ( ! $this->upload->do_upload('userfile'))
-                {
-                        $error = array('error' => $this->upload->display_errors());
-
-                        $this->load->view('upload_form', $error);
-                }
-                else
-                {
-                        $data = array('upload_data' => $this->upload->data());
-
-                        $this->load->view('upload_success', $data);
-                }		
 	}
 
 }
