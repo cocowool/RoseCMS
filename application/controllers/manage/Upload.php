75656079b4156dcf0f38cbbc2d883339224ee6a7
Green Wang <cocowool@qq.com>
2017-08-08 09:58:43 +0800

    Test sae storage

--- application/controllers/manage/Upload.php
+++ application/controllers/manage/Upload.php
@@ -22,12 +22,13 @@ class Upload extends MY_Controller {
 		$secret_key = "0j5xylxkh4h52h242hj0jhm44xzlyll3025iw2wm";
 
 		$s = new sinacloud\sae\Storage($access_key, $secret_key);
+		$s->listBuckets();
 		var_dump($s);
 
 		die("Sina Storage Test<br />");
 
 		$config['upload_path'] = 'upload/' . date('Ymd', time());
-		if(!is_dir($config['upload_path']))	mkdir($config['upload_path'], 0777);
+		//if(!is_dir($config['upload_path']))	mkdir($config['upload_path'], 0777);
 		//$config['file_name'] = 'drill_' . $data['group'] . '_' . $data['position'] . '_' . time();
         $config['allowed_types'] = 	'gif|jpg|png';
         $config['max_size'] = 0;
@@ -35,7 +36,7 @@ class Upload extends MY_Controller {
         $config['max_width'] = 0;
 
 		$this->load->library('upload', $config);
-		if(!$this->upload->do_upload('file')){
+		if(!$this->upload->sae_upload('file', 'thumbnail', $s)){
 			print_r($this->upload->display_errors());
 			die('{"jsonrpc" : "2.0", "error" : {"code": 101, "message": "Failed to open input stream."}, "id" : "id"}');
 		}else{
