e24717eaa83827b52d1dddc0c2d1d08395b1b4b9
Green Wang <cocowool@qq.com>
2017-11-17 08:37:50 +0800

    Add Article List

--- application/views/manage/article/article_list.php
+++ application/views/manage/article/article_list.php
@@ -3,7 +3,7 @@
 		<h1 class="rs-head">文章管理</h1>
 		<?php //echo validation_errors(); ?>
 		<div id="rs-table-container">
-			<table class="stripped">
+			<table class="table table-striped table-hover">
 				<tr>
 					<th>标题</th>
 					<th>作者</th>
@@ -11,6 +11,7 @@
 					<th>标签</th>
 					<th>评论</th>
 					<th>日期</th>
+					<th>操作</th>
 				</tr>
 				<?php
 				$html = '';
@@ -21,10 +22,11 @@
 					$html .= '<td></td>';
 					$html .= '<td></td>';
 					$html .= '<td></td>';
-					$html .= '<td></td>';
 					$html .= '<td>' . $value['post_date'] . '</td>';
+					$html .= '<td><a href="/manage/article/add/' . $value['id'] . '">编辑</a>&nbsp;&nbsp;<a href="/manage/article/del/' . $value['id'] . '">删除</a></td>';
 					$html .= '</tr>';
 				}
+
 				echo $html;
 				?>
 			</table>
