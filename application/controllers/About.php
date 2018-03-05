8f65d608d2719c3b03e5669844cba627d062eb31
Green Wang <cocowool@qq.com>
2018-03-05 18:45:34 +0800

    modify menu tip

--- application/controllers/About.php
+++ application/controllers/About.php
@@ -7,6 +7,13 @@ class About extends CI_Controller {
 	{
 		$data = array();
 
+		// 判断用户是否登陆
+		if( $this->session->userdata('rs_user_login') ){
+			$data['user_login'] = $this->session->userdata('rs_user_login');
+		}else{
+			$data['user_login'] = '';
+		}
+
 		$this->load->view('about', $data);
 	}
 }
--- application/controllers/Question.php
+++ application/controllers/Question.php
@@ -17,6 +17,13 @@ class Question extends MY_Controller {
 		$option[] = array('data' =>	'open', 'field' => 'q_status', 'action' => 'where'	);
 		$data['question_list'] = $this->q->getAll( $option, $page, $pagesize = 10, 'id', 'desc' );
 
+		// 判断用户是否登陆
+		if( $this->session->userdata('rs_user_login') ){
+			$data['user_login'] = $this->session->userdata('rs_user_login');
+		}else{
+			$data['user_login'] = '';
+		}
+
 		$this->load->view('question_list', $data);		
 	}
 
@@ -26,6 +33,13 @@ class Question extends MY_Controller {
 		$this->load->model('Question_Model', 'q');
 		$option = array();
 		$data['question'] = $this->q->getById($id);
+
+		// 判断用户是否登陆
+		if( $this->session->userdata('rs_user_login') ){
+			$data['user_login'] = $this->session->userdata('rs_user_login');
+		}else{
+			$data['user_login'] = '';
+		}
 
 		$this->load->view('question_detail', $data);
 	}
