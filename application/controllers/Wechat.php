d2f869e50754e41cf38058674abf99e5b2101526
Green Wang <cocowool@qq.com>
2018-03-31 22:31:30 +0800

    test wechat api

--- application/controllers/Wechat.php
+++ application/controllers/Wechat.php
@@ -50,7 +50,7 @@ class Wechat extends CI_Controller {
 			switch ($msg_type) {
 				case 'text':
 					$keyword = trim($post_obj->Content);
-					$msg_tpl = '<xml> <ToUserName><![CDATA[%s] ]></ToUserName> <FromUserName><![CDATA[%s] ]></FromUserName> <CreateTime>%s</CreateTime> <MsgType><![CDATA[text] ]></MsgType> <Content><![CDATA[%s] ]></Content> </xml>';
+					$msg_tpl = '<xml><ToUserName><![CDATA[%s]]></ToUserName><FromUserName><![CDATA[%s]]></FromUserName><CreateTime>%s</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[%s]]></Content></xml>';
 					$result =  sprintf($msg_tpl,$post_obj->FromUserName, $post_obj->ToUserName, time(), '欢迎访问大江小浪!');
 					echo $result;
 					return;
