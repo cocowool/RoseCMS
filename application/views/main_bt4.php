bc0a73cae67820dd97f504eaf7c4582ba2d02728
Green Wang <cocowool@qq.com>
2018-04-23 15:06:34 +0800

    modify style

--- application/views/main_bt4.php
+++ application/views/main_bt4.php
@@ -85,8 +85,34 @@
 		</div>
 		
 	</div>
-	<div class="container" id="rs-footer">
-		
+	<div class="container-fluid  bg-dark" id="rs-footer">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-2">
+					<div class="rs-ft-about">
+						<p><img src="/static/default/image/sapublic.jpg" width="129px"></p>					
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="rs-ft-about text-light">
+						<h5>关于我们</h5>
+						<p>我们致力于分享与计算机技术与软件专业技术资格（水平）考试相关的所有信息，如有需要，关注我们的微信公众号，随时随地掌握更多内容。</p>
+						<p>Copyright &copy; 2018 京ICP备15058613－1号</p>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="rs-ft-about text-light">
+						<h5>友情链接</h5>
+						<ul>
+							<li><a href="http://www.ruankao.org.cn/jsjnew/cms/focusNews/">中国计算机技术职业资格网</a></li>
+							<li><a href="http://www.miit.gov.cn">中华人民共和国工业和信息化部</a></li>
+							<li><a href="http://cnblogs.com/cocowool">小狼的世界</a></li>
+						</ul>					
+					</div>				
+				</div>
+			</div>
+			
+		</div>
 	</div>
 	<script type="text/javascript" src="/static/lib/vue-2.3.0/vue.js"></script>
 	<script src="/static/lib/jquery/jquery-3.2.1.min.js" type="text/javascript"></script>
