dae0acfe0e129e6f208cb313a0e95e4c7b38cebe
Green Wang <cocowool@qq.com>
2018-04-23 09:29:02 +0800

    add page

--- application/views/main_bt4.php
+++ application/views/main_bt4.php
@@ -60,6 +60,17 @@
 				?>				
 					</ul>
 				</div>
+				<div id="rs-question-page">
+					<nav aria-label="Page navigation example">
+						<ul class="pagination">
+							<li class="page-item"><a class="page-link" href="#">上一页</a></li>
+							<li class="page-item"><a class="page-link" href="#">1</a></li>
+							<li class="page-item"><a class="page-link" href="#">2</a></li>
+							<li class="page-item"><a class="page-link" href="#">3</a></li>
+							<li class="page-item"><a class="page-link" href="#">下一页</a></li>
+						</ul>
+					</nav>
+				</div>
 			</div>
 			<div class="col-md-4">
 				<div id="rs-google-ad">
