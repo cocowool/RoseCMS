586e314655779fd20714a7e74f5146e7f101d627
Green Wang <cocowool@qq.com>
2018-07-18 19:14:16 +0800

    modify tip page

--- application/views/tip.php
+++ application/views/tip.php
@@ -1,16 +1,14 @@
-<!DOCTYPE html>
-<html>
+<!doctype html>
+<html lang="zh-CN">
 <head>
-	<meta charset='UTF-8'>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" href="/static/lib/bootstrap-4.1.0/css/bootstrap-reboot.min.css">
+	<link rel="stylesheet" href="/static/lib/bootstrap-4.1.0/css/bootstrap.min.css">
+	<link rel="stylesheet" href="/source/main.css">
 	<title>软考资料站</title>
-	<meta name="keyword" content="软考，系统分析师，系统架构师">
-	<meta name="description" content="系统分析师资料站">
-	<link href="/static/lib/bootstrap-3.3.7/css/bootstrap.min.css" rel="stylesheet">
-	<link rel='stylesheet' id='twentyseventeen-style-css'  href='/static/css/main.css' type='text/css' media='all' />
-	<script type="text/javascript" src="/static/lib/vue-2.3.0/vue.js"></script>
-	<script type="text/javascript" src="/static/lib/jquery/jquery-3.2.1.min.js"></script>
+	<meta name="keyword" content="软考，系统分析师，系统架构师，真题">
+	<meta name="description" content="软考资料分享，软考真题模拟">
 	<script>
 	var _hmt = _hmt || [];
 	(function() {
@@ -22,14 +20,36 @@
 	</script>
 </head>
 <body class="">
-	<div class="container rs-top-menu">
+	<nav id="rs-nav" class="navbar navbar-expand-sm navbar-dark bg-primary sticky-top">
+		<a href="/" class="navbar-brand text-light"><img src="/static/default/image/sa_white.png" width="30" height="30">&nbsp;&nbsp;软考资料</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#rs-nav-menu" aria-controls="rs-nav-menu" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+
+		<div class="collapse navbar-collapse" id="rs-nav-menu">
+			<ul class="navbar-nav">
+				<li class="nav-item active"><a href="/" class="nav-link">文章</a></li>
+				<li class="nav-item"><a href="/question.html" class="nav-link">真题</a></li>
+				<li class="nav-item"><a href="/about.html" class="nav-link">关于</a></li>
+			</ul>
+		</div>
+
+		<div class="form-inline my-2 my-lg-0" id="rs-tr-container">
 		<?php
-			$this->load->view('menu');
+			if( $this->session->userdata('rs_user_login') ){
+				echo '<div class="text-light">' . $this->session->userdata('rs_user_nicename') . ' 欢迎回来，<a class="text-light" href="/logout.html">退出</a></div>';
+			}else{
 		?>
-	</div>
-	<div class="rs-body container">
+			<a href="/login.html" data-toggle="modal" data-target="#rs_login_modal" class="btn btn-sm btn-link collapse navbar-collapse text-light">登录</a>
+			<a href="/register.html" data-toggle="modal" data-target="#rs_register_modal"  class="btn btn-sm btn-link collapse navbar-collapse text-light">注册</a>
+		<?php
+			}
+		?>
+		</div>
+	</nav>
+	<div id="rs-content" class="container">
 		<div class="row">
-			<div class="col-md-9">
+			<div class="col-md-8">
 				<div class="row">
 					<div class="col-md-1"></div>
 					<div class="rs-register-form col-md-6">
@@ -48,23 +68,46 @@
 					
 				</div>
 			</div>
-			<div class="col-md-3" id="right-column">
-				<div class="rs-google-ad" id="google-ad">
+			<div class="col-md-4">
+				<div id="rs-google-ad">
 					<?php $this->load->view('adsense'); ?>					
-				</div>			
-				<div class="rs-sidebox" id="date_counter">
-					<div class="rs-date-counter">
-						<p>距离2018年软考还有<span id="dateCounter">{{ dayCount }}</span>天</p>
-						<p></p>
+				</div>
+				<div class="bg-primary text-light p-1 align-middle text-center d-flex my-2 m-y-2 py-1 p-y-1" id="date_counter">
+					<div class="rs-date-counter align-middle text-center p-2">
+						<p class="m-0 text-center">距离2018年软考还有<span id="dateCounter">{{ dayCount }}</span>天</p>
 					</div>
 				</div>
 			</div>
 		</div>
 	</div>
-	<div class="rs-footer">
-	<?php
-		$this->load->view('footer');
-	?>
+	<div class="container-fluid  bg-dark pt-4 p-t-4" id="rs-footer">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-2">
+					<div class="rs-ft-about">
+						<p class="text-center text-md-left"><img src="/static/default/image/sapublic.jpg" width="129px"></p>					
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="rs-ft-about text-center text-sm-center text-md-left text-light">
+						<h5>关于我们</h5>
+						<p>本站致力于分享与计算机技术与软件专业技术资格（水平）考试相关的所有信息，为所有软考路上的朋友提供所需帮助，您可以关注我们的微信公众号，进行随时的自我测验以及与更多的考友一起交流。</p>
+						<p>Copyright &copy; 2018 京ICP备15058613－1号</p>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="rs-ft-about text-light text-center text-sm-center text-md-left">
+						<h5>友情链接</h5>
+						<ul class="rs_friendlink">
+							<li><a href="http://www.ruankao.org.cn/jsjnew/cms/focusNews/">中国计算机技术职业资格网</a></li>
+							<li><a href="http://www.miit.gov.cn">中华人民共和国工业和信息化部</a></li>
+							<li><a href="http://cnblogs.com/cocowool">小狼的世界</a></li>
+						</ul>					
+					</div>				
+				</div>
+			</div>
+			
+		</div>
 	</div>
 </body>
 <script type="text/javascript">
@@ -73,7 +116,7 @@
 	});
 
 	var d = new Date();
-	var se = new Date('2018-05-26');
+	var se = new Date('2018-11-10');
 	var ddCount = parseInt( (Math.abs(se - d))/1000/60/60/24 ); 
 
 	var vm = new Vue({
